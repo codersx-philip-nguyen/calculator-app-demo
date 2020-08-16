ddd167e447aee7251e822f69d23badbeb08e864d
Philip Ng <indieretro20s@gmail.com>
2020-08-16 20:59:12 +0700

    Updates

---
+++ precache-manifest.d7dedc3e210659596a4d0cd804240311.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af31bfa0570a0f50fdc828e3d68d38b9",
+    "url": "/calculator-app-demo/index.html"
+  },
+  {
+    "revision": "b39b38387ca71650bb2a",
+    "url": "/calculator-app-demo/static/css/main.0311fa24.chunk.css"
+  },
+  {
+    "revision": "957638ea8982320dc693",
+    "url": "/calculator-app-demo/static/js/2.e3675be0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/calculator-app-demo/static/js/2.e3675be0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b39b38387ca71650bb2a",
+    "url": "/calculator-app-demo/static/js/main.f3870c9e.chunk.js"
+  },
+  {
+    "revision": "4aace58d7e95743db70c",
+    "url": "/calculator-app-demo/static/js/runtime-main.be65f20c.js"
+  }
+]);
